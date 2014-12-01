7ddaa5c2a519598d162cb54569c34ab24b7923ec
Yuchen Yang <jasonyangftd@gmail.com>
2014-11-30 19:02:15 -0800

    tests passed, but a little timer appears on getElementsByClassName, don't know what's going on

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+	var result = [];
+    var test = function(element) {
+        var testList = element.childNodes;
+		
+		if(element.classList === undefined){ 
+			return;
+		}
+		
+        for (var i = 0; i < element.classList.length; i++) {
+            if (element.classList[i] === className) {
+                result.push(element);
+            }
+        }
+		
+		for (var i = 0; i < testList.length; i++){
+			test(testList[i]);
+		}
+    }
+    test(document.body);
+    return result;  
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,29 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+// but you don't so you're going to have to write it from scratch:
+var stringifyJSON = function (obj) {
+  var result = '';
+	if( obj === null ) {
+		return 'null';
+	} else if ( obj === undefined ) {
+		return undefined;
+	} else if( obj === true || obj === false || typeof(obj) === 'number' ){
+		return obj.toString();
+	} else if( typeof(obj) === 'string' ) {
+		return '"' + obj + '"';
+	} else if( Array.isArray(obj) ) {
+		result = [];
+		for(var i = 0; i < obj.length; i++) {
+			result.push(stringifyJSON(obj[i]));
+		}
+		return '[' + result + ']';
+	} else if( typeof obj === 'object' ) {
+		for( var key in obj ) {
+			if( stringifyJSON(obj[key]) !== undefined || null ) {
+				result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+			}
+		}
+		return '{' + result.slice(0, result.length - 1) + '}';
+	}
 };
